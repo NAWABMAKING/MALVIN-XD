a9fc63758d925ba0cd6761f46b58a9a32c496318
NAWABMAKING <papaidas4051@gmail.com>
2025-08-15 06:43:05 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~s6UFwZiK#iZ9WuyG19Q8SFbeDL7Gi2chz_xPHqUG9_UzyersUbnI",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
